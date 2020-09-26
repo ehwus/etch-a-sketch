4caad1c3689b734f009bc9f5f7a7831443224023
alex@awithsmith.com <alex@awithsmith.com>
2020-09-26 06:43:03 +0100

    change background to random raindow color on mouseover

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
+const RAINBOW = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
 // build the grid
+const grid = document.querySelector('.grid')
 for (let i = 0; i < 256; i++) {
     console.log(i)
     let square = document.createElement('div');
     square.className = "square";
-    document.getElementsByClassName('grid')[0].appendChild(square);
+    grid.appendChild(square);
+}
+
+// listen for mouseover
+const squares = document.querySelectorAll('.square')
+for (const square of squares) {
+    square.addEventListener("mouseover", function() {
+        square.style.background = RAINBOW[Math.floor(Math.random() * Math.floor(6))];
+    });
 }
