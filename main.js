2eb87af232f28be4e4085b2b2c9b15ff3bf0a11e
alex@awithsmith.com <alex@awithsmith.com>
2020-09-26 07:15:43 +0100

    added reset button

--- main.js
+++ main.js
@@ -1,18 +1,36 @@
+// CONSTANTS
 const RAINBOW = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const GRID = document.querySelector('.grid')
+const reset = document.getElementById("reset");
 
+// FUNCTIONS
 // build the grid
-const grid = document.querySelector('.grid')
-for (let i = 0; i < 256; i++) {
-    console.log(i)
-    let square = document.createElement('div');
-    square.className = "square";
-    grid.appendChild(square);
+function buildGrid(size) {
+    for (let i = 0; i < size ** 2; i++) {
+        let square = document.createElement('div');
+        square.className = "square";
+        GRID.appendChild(square);
+    }
+    enableGrid()
+}
+// listen for mouseover
+function enableGrid() {
+    const squares = document.querySelectorAll('.square')
+    for (const square of squares) {
+        square.addEventListener("mouseover", function () {
+            square.style.background = RAINBOW[Math.floor(Math.random() * Math.floor(6))];
+        });
+    }
+}
+// reset the grid
+function resetGrid() {
+    GRID.innerHTML = "";
+    buildGrid(16);
 }
 
-// listen for mouseover
-const squares = document.querySelectorAll('.square')
-for (const square of squares) {
-    square.addEventListener("mouseover", function() {
-        square.style.background = RAINBOW[Math.floor(Math.random() * Math.floor(6))];
-    });
-}
+// MAIN
+buildGrid(16)
+// listen for reset
+reset.addEventListener("click", function() {
+    resetGrid();
+});
